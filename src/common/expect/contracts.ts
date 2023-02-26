34b9cd2389f473a0467d6d254d826713be33b64f
Nikolay Makhonin <nikolay.makhonin@gmail.com>
2023-02-26 16:55:30 +0400

    dev: match tool

--- src/common/expect/contracts.ts
+++ src/common/expect/contracts.ts
@@ -97,3 +97,47 @@ export function isArrayContainingNItems<T>(item: T, count: number): MatcherSync<
     () => `array containing ${count} items ${item}`,
   )
 }
+
+export function isArrayContainingNItemsAsync<T>(item: T, count: number): MatcherAsync<T[]> {
+  return new Matcher(
+    true,
+    async (actual: T[]) => {
+      if (!Array.isArray(actual)) {
+        return 'is not an array'
+      }
+      const len = actual.length
+      if (len < count) {
+        return `array length < ${count}`
+      }
+      const nestedTrue: MatchResultNested[] = []
+      const nestedFalse: MatchResultNested[] = []
+      let matchCount = 0
+      for (let i = 0; i < len; i++) {
+        const result = await matchAsync(actual[i], item)
+        nestedFalse.push({
+          key: i,
+          result,
+        })
+        if (result.result) {
+          matchCount++
+          nestedTrue.push({
+            key: i,
+            result,
+          })
+        }
+        if (matchCount >= count) {
+          return {
+            result: true,
+            nested: nestedTrue,
+          }
+        }
+      }
+      return {
+        result: false,
+        cause : `array length >= ${count} but not enough items match`,
+        nested: nestedFalse,
+      }
+    },
+    () => `array containing ${count} items ${item}`,
+  )
+}
