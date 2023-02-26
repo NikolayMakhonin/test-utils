c3995ac7e46de152a261e38ca5510e2c05cc2c20
Nikolay Makhonin <nikolay.makhonin@gmail.com>
2023-02-26 20:19:49 +0400

    dev: match tool

--- src/common/expect/contracts.ts
+++ src/common/expect/contracts.ts
@@ -54,90 +54,90 @@ export type Expected<T> = T | MatcherSync<T> | MatcherAsync<T>
 export type ExpectedSync<T> = T | MatcherSync<T>
 export type ExpectedAsync<T> = T | MatcherAsync<T>
 
-export function isArrayContainingNItems<T>(item: T, count: number): MatcherSync<T[]> {
-  return new Matcher(
-    false,
-    (actual: T[]) => {
-      if (!Array.isArray(actual)) {
-        return 'is not an array'
-      }
-      const len = actual.length
-      if (len < count) {
-        return `array length < ${count}`
-      }
-      const nestedTrue: MatchResultNested[] = []
-      const nestedFalse: MatchResultNested[] = []
-      let matchCount = 0
-      for (let i = 0; i < len; i++) {
-        const result = matchSync(actual[i], item)
-        nestedFalse.push({
-          key: i,
-          result,
-        })
-        if (result.result) {
-          matchCount++
-          nestedTrue.push({
-            key: i,
-            result,
-          })
-        }
-        if (matchCount >= count) {
-          return {
-            result: true,
-            nested: nestedTrue,
-          }
-        }
-      }
-      return {
-        result: false,
-        cause : `array length >= ${count} but not enough items match`,
-        nested: nestedFalse,
-      }
-    },
-    () => `array containing ${count} items ${item}`,
-  )
-}
-
-export function isArrayContainingNItemsAsync<T>(item: T, count: number): MatcherAsync<T[]> {
-  return new Matcher(
-    true,
-    async (actual: T[]) => {
-      if (!Array.isArray(actual)) {
-        return 'is not an array'
-      }
-      const len = actual.length
-      if (len < count) {
-        return `array length < ${count}`
-      }
-      const nestedTrue: MatchResultNested[] = []
-      const nestedFalse: MatchResultNested[] = []
-      let matchCount = 0
-      for (let i = 0; i < len; i++) {
-        const result = await matchAsync(actual[i], item)
-        nestedFalse.push({
-          key: i,
-          result,
-        })
-        if (result.result) {
-          matchCount++
-          nestedTrue.push({
-            key: i,
-            result,
-          })
-        }
-        if (matchCount >= count) {
-          return {
-            result: true,
-            nested: nestedTrue,
-          }
-        }
-      }
-      return {
-        result: false,
-        cause : `array length >= ${count} but not enough items match`,
-        nested: nestedFalse,
-      }
-    },
-    () => `array containing ${count} items ${item}`,
-  )
-}
+// export function isArrayContainingNItems<T>(item: T, count: number): MatcherSync<T[]> {
+//   return new Matcher(
+//     false,
+//     (actual: T[]) => {
+//       if (!Array.isArray(actual)) {
+//         return 'is not an array'
+//       }
+//       const len = actual.length
+//       if (len < count) {
+//         return `array length < ${count}`
+//       }
+//       const nestedTrue: MatchResultNested[] = []
+//       const nestedFalse: MatchResultNested[] = []
+//       let matchCount = 0
+//       for (let i = 0; i < len; i++) {
+//         const result = matchSync(actual[i], item)
+//         nestedFalse.push({
+//           key: i,
+//           result,
+//         })
+//         if (result.result) {
+//           matchCount++
+//           nestedTrue.push({
+//             key: i,
+//             result,
+//           })
+//         }
+//         if (matchCount >= count) {
+//           return {
+//             result: true,
+//             nested: nestedTrue,
+//           }
+//         }
+//       }
+//       return {
+//         result: false,
+//         cause : `array length >= ${count} but not enough items match`,
+//         nested: nestedFalse,
+//       }
+//     },
+//     () => `array containing ${count} items ${item}`,
+//   )
+// }
+//
+// export function isArrayContainingNItemsAsync<T>(item: T, count: number): MatcherAsync<T[]> {
+//   return new Matcher(
+//     true,
+//     async (actual: T[]) => {
+//       if (!Array.isArray(actual)) {
+//         return 'is not an array'
+//       }
+//       const len = actual.length
+//       if (len < count) {
+//         return `array length < ${count}`
+//       }
+//       const nestedTrue: MatchResultNested[] = []
+//       const nestedFalse: MatchResultNested[] = []
+//       let matchCount = 0
+//       for (let i = 0; i < len; i++) {
+//         const result = await matchAsync(actual[i], item)
+//         nestedFalse.push({
+//           key: i,
+//           result,
+//         })
+//         if (result.result) {
+//           matchCount++
+//           nestedTrue.push({
+//             key: i,
+//             result,
+//           })
+//         }
+//         if (matchCount >= count) {
+//           return {
+//             result: true,
+//             nested: nestedTrue,
+//           }
+//         }
+//       }
+//       return {
+//         result: false,
+//         cause : `array length >= ${count} but not enough items match`,
+//         nested: nestedFalse,
+//       }
+//     },
+//     () => `array containing ${count} items ${item}`,
+//   )
+// }
