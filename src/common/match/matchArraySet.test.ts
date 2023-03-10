2ae2dae0778a91f0ff588076f56d69c577aa9391
Nikolay Makhonin <nikolay.makhonin@gmail.com>
2023-03-10 11:11:35 +0400

    dev: match tool

--- src/common/match/matchArraySet.test.ts
+++ src/common/match/matchArraySet.test.ts
@@ -1,4 +1,6 @@
+/* eslint-disable array-element-newline */
 import {createTestVariants} from '@flemist/test-variants'
+import {calcPerformance} from 'rdtsc'
 
 type MatchArraySetOptions = {
   mayNotContains?: boolean
@@ -7,9 +9,9 @@ type MatchArraySetOptions = {
   expectedRepeats?: boolean
 }
 
-function matchArraySet(
-  actual: number[], expected: number[],
-  match: (actual: number, expected: number) => boolean,
+function matchArraySetOld<T>(
+  actual: T[], expected: T[],
+  match: (actual: T, expected: T) => boolean,
   options: MatchArraySetOptions,
 ): boolean {
   if (options?.mayNotContains && options?.mayNotContained) {
@@ -134,6 +136,196 @@ function matchArraySet(
   return true
 }
 
+function matchArraySet<T>(
+  actual: T[], expected: T[],
+  match: (actual: T, expected: T) => boolean,
+  options: MatchArraySetOptions,
+): boolean {
+  if (options?.mayNotContains && options?.mayNotContained) {
+    throw new Error(`At least one of the options 'mayNotContains' or 'mayNotContained' should be false`)
+  }
+
+  let expectedMap: Map<T, number>
+  for (let i = 0, len = expected.length; i < len; i++) {
+    const expectedItem = expected[i]
+    let count: number
+    if (!expectedMap) {
+      expectedMap = new Map()
+      count = 1
+    }
+    else {
+      count = (expectedMap.get(expectedItem) || 0) + 1
+    }
+    expectedMap.set(expectedItem, count)
+  }
+
+  let actualMap: Map<T, number>
+  for (let i = 0, len = actual.length; i < len; i++) {
+    const actualItem = actual[i]
+    let count: number
+    if (!actualMap) {
+      actualMap = new Map()
+      count = 1
+    }
+    else {
+      count = (actualMap.get(actualItem) || 0) + 1
+    }
+    actualMap.set(actualItem, count)
+  }
+
+  if (!options?.mayNotContains) {
+    actualMap?.forEach((actualCount, actualItem) => {
+      let expectedCount = expectedMap?.get(actualItem)
+      if (expectedCount) {
+        const minCount = Math.min(actualCount, expectedCount)
+        expectedCount -= minCount
+        actualCount -= minCount
+        expectedMap.set(actualItem, expectedCount)
+      }
+      actualMap.set(actualItem, actualCount)
+    })
+
+    actualMap?.forEach((actualCount, actualItem) => {
+      if (!actualCount) {
+        return
+      }
+      while (actualCount > 0) {
+        let found = false
+        for (const [expectedItem, expectedCount] of expectedMap) {
+          if (expectedCount > 0 && match(actualItem, expectedItem)) {
+            expectedMap.set(expectedItem, expectedCount - 1)
+            actualCount--
+            found = true
+            break
+          }
+        }
+        if (!found) {
+          break
+        }
+      }
+      actualMap.set(actualItem, actualCount)
+    })
+  }
+
+  if (!options?.mayNotContained) {
+    expectedMap?.forEach((expectedCount, expectedItem) => {
+      let actualCount = actualMap?.get(expectedItem)
+      if (actualCount) {
+        const minCount = Math.min(actualCount, expectedCount)
+        expectedCount -= minCount
+        actualCount -= minCount
+        actualMap.set(expectedItem, actualCount)
+      }
+      expectedMap.set(expectedItem, expectedCount)
+    })
+
+    expectedMap?.forEach((expectedCount, expectedItem) => {
+      if (!expectedCount) {
+        return
+      }
+      while (expectedCount > 0) {
+        let found = false
+        for (const [actualItem, actualCount] of actualMap) {
+          if (actualCount > 0 && match(actualItem, expectedItem)) {
+            actualMap.set(actualItem, actualCount - 1)
+            expectedCount--
+            found = true
+            break
+          }
+        }
+        if (!found) {
+          break
+        }
+      }
+      expectedMap.set(expectedItem, expectedCount)
+    })
+  }
+
+  let actualMapHasItems = false
+  for (const [actualItem, actualCount] of actualMap) {
+    if (actualCount) {
+      actualMapHasItems = true
+      break
+    }
+  }
+
+  let expectedMapHasItems = false
+  for (const [expectedItem, expectedCount] of expectedMap) {
+    if (expectedCount) {
+      expectedMapHasItems = true
+      break
+    }
+  }
+
+  if (actualMapHasItems) {
+    if (options?.mayNotContained) {
+      return true
+    }
+
+    if (options?.actualRepeats) {
+      for (let [actualItem, actualCount] of actualMap) {
+        if (!actualCount) {
+          continue
+        }
+
+        if (expectedMap?.get(actualItem) === 0) {
+          continue
+        }
+
+        let found = false
+        for (const [expectedItem, expectedCount] of expectedMap) {
+          if (!expectedCount && match(actualItem, expectedItem)) {
+            found = true
+            break
+          }
+        }
+        if (!found) {
+          return false
+        }
+        actualCount--
+      }
+    }
+    else {
+      return false
+    }
+  }
+
+  if (expectedMapHasItems) {
+    if (options?.mayNotContains) {
+      return true
+    }
+
+    if (options?.expectedRepeats) {
+      for (let [expectedItem, expectedCount] of expectedMap) {
+        if (!expectedCount) {
+          continue
+        }
+
+        if (actualMap?.get(expectedItem) === 0) {
+          continue
+        }
+
+        let found = false
+        for (const [actualItem, actualCount] of actualMap) {
+          if (!actualCount && match(actualItem, expectedItem)) {
+            found = true
+            break
+          }
+        }
+        if (!found) {
+          return false
+        }
+        expectedCount--
+      }
+    }
+    else {
+      return false
+    }
+  }
+
+  return true
+}
+
 function addRepeats(arr: number[], index: number, count: number) {
   const result = [...arr]
   for (let i = 0; i < count; i++) {
@@ -151,7 +343,13 @@ function addBreaks(arr: number[], index: number, count: number) {
 }
 
 describe('matchArraySet', function () {
-  function match(actual: number, expected: number) {
+  function match(actual: any, expected: any) {
+    if (typeof actual === 'object') {
+      actual = actual.value
+    }
+    if (typeof expected === 'object') {
+      expected = expected.value
+    }
     return actual === expected
   }
 
@@ -164,8 +362,8 @@ describe('matchArraySet', function () {
     actualRepeats,
     expectedRepeats,
   }: {
-    actual: number[]
-    expected: number[]
+    actual: any[]
+    expected: any[]
     result: boolean
     mayNotContains?: boolean
     mayNotContained?: boolean
@@ -199,6 +397,7 @@ describe('matchArraySet', function () {
       expectedRepeatsValues: number[]
       mayNotContainsValues: number[]
       mayNotContainedValues: number[]
+      expectedShuffle: number[]
     }>({
       result         : [true, false],
       mayNotContains : [false, true],
@@ -219,7 +418,7 @@ describe('matchArraySet', function () {
           ...!actualRepeats ? ['actualRepeats'] : [],
           ...!expectedRepeats ? ['expectedRepeats'] : [],
         ],
-      values      : () => [[], [1], [1, 2], [1, 2, 1], [1, 2, 1, 2], [1, 2, 3]],
+      values      : () => [[], [1], [1, 2], [1, 2, 1], [1, 2, 1, 2], [1, 2, 3], [1, 1, 2, 2]],
       actualValues: ({
         result, resultFalseType, values, mayNotContains, mayNotContained, expectedRepeats,
       }) => values.length === 0 ? [] : [
@@ -314,8 +513,97 @@ describe('matchArraySet', function () {
           ]
           : [],
       ],
-      actual  : ({mayNotContainedValues: values}) => [values],
-      expected: ({mayNotContainsValues: values}) => [values],
+      expectedShuffle: ({mayNotContainsValues: values}) => [
+        values,
+        values.reverse(),
+        values.sort((a, b) => Math.random() - 0.5),
+      ],
+      actual: ({mayNotContainedValues: values}) => [
+        values,
+        values.map((o, i) => i % 2 === 0 ? o : {value: o}),
+        values.map((o, i) => i % 2 !== 0 ? o : {value: o}),
+        values.map(o => ({value: o})),
+      ],
+      expected: ({mayNotContainsValues: values}) => [
+        values,
+        values.map((o, i) => i % 2 === 0 ? o : {value: o}),
+        values.map((o, i) => i % 2 !== 0 ? o : {value: o}),
+        values.map(o => ({value: o})),
+      ],
     })()
+  })
+
+  it('perf', function () {
+    const actual = [
+      {value: 1}, 2, 4,
+      1, {value: 2}, 3,
+      1, 2, {value: 3},
+      {value: 1}, 2, 3,
+      1, {value: 2}, 3,
+      1, 2, {value: 3},
+      {value: 1}, 2, 3,
+      1, {value: 2}, 3,
+      1, 2, {value: 3},
+    ]
+    const expected = [
+      1, {value: 2}, 3,
+      1, 2, {value: 4},
+      {value: 1}, 2, 4,
+      1, {value: 2}, 4,
+      1, 2, {value: 4},
+      {value: 1}, 2, 4,
+      1, {value: 2}, 4,
+      1, 2, {value: 4},
+      {value: 1}, 2, 4,
+    ]
+    const result = calcPerformance(
+      1000,
+      () => {
+
+      },
+      () => {
+        matchArraySetOld(actual, expected, match, {
+          mayNotContains : true,
+          mayNotContained: false,
+          actualRepeats  : true,
+          expectedRepeats: true,
+        })
+        matchArraySetOld(actual, expected, match, {
+          mayNotContains : false,
+          mayNotContained: true,
+          actualRepeats  : true,
+          expectedRepeats: true,
+        })
+        matchArraySetOld(actual, expected, match, {
+          mayNotContains : false,
+          mayNotContained: false,
+          actualRepeats  : true,
+          expectedRepeats: true,
+        })
+      },
+      () => {
+        matchArraySet(actual, expected, match, {
+          mayNotContains : true,
+          mayNotContained: false,
+          actualRepeats  : true,
+          expectedRepeats: true,
+        })
+        matchArraySet(actual, expected, match, {
+          mayNotContains : false,
+          mayNotContained: true,
+          actualRepeats  : true,
+          expectedRepeats: true,
+        })
+        matchArraySet(actual, expected, match, {
+          mayNotContains : false,
+          mayNotContained: false,
+          actualRepeats  : true,
+          expectedRepeats: true,
+        })
+      },
+    )
+
+    console.log('perf iterations: ' + result.calcInfo.iterations)
+    console.log(Math.round((result.relativeDiff[0] - 1) * 100) + '%')
   })
 })
